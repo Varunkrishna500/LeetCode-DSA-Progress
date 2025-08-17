d31c3004a7bba02d59e19bcf694ea2f8db7373a1
Varunkrishna500 <varunkrishna452@gmail.com>
2025-08-17 20:55:09 +0530

    Create Two Sum problem folder with README

--- TwoSum.java
+++ TwoSum.java
@@ -1,26 +1,20 @@
+import java.util.Arrays;
 public class TwoSum {
-    public int[] twoSum(int[] nums, int target) {
-        int n = nums.length;
-        
-        // Check every possible pair
-        for (int i = 0; i < n; i++) {
-            for (int j = i + 1; j < n; j++) {
+    public static int[] twosum(int[] nums, int target) {
+        int x = nums.length;
+        for (int i = 0; i < x; i++) {
+            for (int j = i + 1; j < x; j++) {
                 if (nums[i] + nums[j] == target) {
-                    return new int[]{i, j};
+                    return new int[] { i, j };
                 }
             }
         }
-        // This shouldn't happen given the problem constraints
-        return new int[]{};
+        return new int[] {};
     }
     public static void main(String[] args) {
-        TwoSum solution = new TwoSum();
-        
-        // Test case 1
-        int[] nums1 = {2, 7, 11, 15};
-        int target1 = 9;
-        int[] result1 = solution.twoSum(nums1, target1);
-        System.out.println("Input: nums = " + Arrays.toString(nums1) + ", target = " + target1);
-        System.out.println("Output: " + Arrays.toString(result1));
+        int[] nums = { 2, 7, 11, 14 };
+        int target = 21;
+        int[] result = twosum(nums, target);
+        System.out.println(Arrays.toString(result));
     }
 }
