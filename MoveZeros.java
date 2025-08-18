bfcb84444970db8864877fe6264eeefbe311d511
Varunkrishna500 <varunkrishna452@gmail.com>
2025-08-18 14:46:19 +0530

    Add Move Zeros Solution with Two pointer Approach

---
+++ MoveZeros.java
@@ -0,0 +1,21 @@
+import java.util.Arrays;
+public class Movezeros {
+    public static void movezeros(int nums[]) {
+        int left = 0; //pointer for placing non zero elements
+        //Iterate with right pointer
+        for (int right = 0; right < nums.length; right++) {
+            if (nums[right] != 0) {
+                //swap if right pointer is non zero
+                int temp = nums[left];
+                nums[left] = nums[right];
+                nums[right] = temp;
+                left++;
+            }
+        }
+    }
+    public static void main(String[] args) {
+        int nums[] = { 0, 1, 0, 3, 12, 0 };
+        movezeros(nums);
+        System.out.println(Arrays.toString(nums));
+    }
+}
